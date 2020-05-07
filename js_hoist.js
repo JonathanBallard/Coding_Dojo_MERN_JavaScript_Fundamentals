655ff6dd8bd57e67c1633b0d866e8f0e5fb5dda4
Jonathan Ballard <asgardschosen@gmail.com>
2020-05-06 19:11:19 -0700

    finished optional

--- js_hoist.js
+++ js_hoist.js
@@ -210,13 +210,29 @@ function makeDojo(name, students){
 */
 
 //8 ANSWER
-
-
+function makeDojo(name, students){
+    const dojo = {};
+    dojo.name = name;
+    dojo.students = students;
+    if(dojo.students > 50){
+        dojo.hiring = true;
+    }
+    else if(dojo.students <= 0){
+        dojo = "closed for now";
+    }
+    return dojo;
+}
+console.log(makeDojo("Chicago", 65));
+console.log(makeDojo("Berkeley", 0));
 
 //8 OUTPUT:
-// 
-// 
-// 
+// dojo = {
+    // name: "Chicago",
+    // students: 65,
+    // hiring: true,
+//}
+// error
+
 
 
 
